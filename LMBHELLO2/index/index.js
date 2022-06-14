307c4b782d59baf7a01aac31fd1fe071c26a91b0
anlusoft <anlusoft4@gmail.com>
2022-06-14 00:57:27 -0500

    envio

---
+++ LMBHELLO2/index/index.js
@@ -0,0 +1,19 @@
+const parse = async (event, context) => {
+
+    console.log(event)
+   
+        return {
+            "statusCode": 200,
+            "body":JSON.stringify({"salida":"exito,"}),
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Strict-Transport-Security": "max-age=31536000; includeSubDomains",
+                "X-Frame-Options": "SAMEORIGIN",
+                "Referrer-Policy": "no-referrer-when-downgrade",
+                "X-Content-Type-Options": "nosniff",
+                "X-XSS-Protection": "1; mode=block"
+            },
+            "isBase64Encoded": false
+          };  
+};
+exports.handler = parse;
